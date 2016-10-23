6ea14f970cdc468112e0585a5688b14ba476cea0
tommytsunamy <tommym147@gmail.com>
2016-10-23 01:18:15 -0700

    Initial commit

---
+++ app/src/main/java/com/example/ace/beeradviser/FindBeerActivity.java
@@ -0,0 +1,45 @@
+package com.example.ace.beeradviser;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
+import java.util.List;
+
+public class FindBeerActivity extends AppCompatActivity
+{
+    private BeerExpert expert = new BeerExpert();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_beer);
+    }
+
+    //Call when the user clicks the button
+    public void onClickFindBeer(View view)
+    {
+        //Get a reference to the TextView
+        TextView brands = (TextView) findViewById(R.id.brands);
+
+        //Get a reference to the Spinner
+        Spinner color = (Spinner) findViewById(R.id.color);
+
+        //Get the selected item in the Spinner
+        String beerType = String.valueOf(color.getSelectedItem());
+
+        //Get recommendations form the BeerExpert class
+        List<String> brandsList = expert.getBrands(beerType);
+
+        StringBuilder brandsFormatted = new StringBuilder();
+        for (String brand : brandsList)
+        {
+            brandsFormatted.append(brand).append('\n');
+        }
+
+        //Display the selected item
+        brands.setText(brandsFormatted);
+    }
+}
